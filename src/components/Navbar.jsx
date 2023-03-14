0131f8584dcc91110eaa62233cf111f90e24b2af
Vadim <vadimsotropa@gmail.coml>
2023-03-14 14:12:09 +0100

    logo picture update and mobile mune style changed + one crypto paged styled for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import logo from "../img/Group 2.png";
+import logo from "../img/Group 4.svg";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 import { useRecoilState } from "recoil";
 import { userStateLogin } from "../pages/Login";
 import axios from "axios";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaRegWindowClose } from "react-icons/fa";
 export default function Navbar() {
   const [user, setUser] = useRecoilState(userStateLogin);
   const [showMenu, setShowMenu] = useState(false);
@@ -124,8 +124,11 @@ export default function Navbar() {
 {showMenu && (
       <div className="mobileMenu">
         <div className="mobileMenuHeader">
+        <Link className={"link-logo"} to={"/"}>
+            <img src={logo} alt="" className="image-logo"/>
+          </Link>
           <button className="closeMobileMenuBtn" onClick={() => setShowMenu(false)}>
-            X
+           <FaRegWindowClose/>
           </button>
         </div>
         <div className="mobileMenuItems">
