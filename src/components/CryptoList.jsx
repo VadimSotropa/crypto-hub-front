78a2b21a7f499008da2a786b99b1bc0eb13c23fd
Vadim <vadimsotropa@gmail.coml>
2023-03-20 00:18:53 +0100

    chart label changed

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -262,7 +262,7 @@ function CryptoItem({ crypto, currency }) {
         datasets: [
           {
             pointStyle: false,
-            label: crypto.name,
+            label: 'price',
             data: coinChartData.map((val) => val.y),
             borderColor: "rgb(53, 162, 235)",
             backgroundColor: "rgba(53, 162, 235, 0.5)",
--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -249,7 +249,7 @@ function CryptoItem() {
       datasets: [
         {
           pointStyle: false,
-          label: crypto.name,
+          label: 'price',
           data: coinChartData.map((val) => val.y),
           borderColor: "rgb(53, 162, 235)",
           backgroundColor: "rgba(53, 162, 235, 0.5)",
